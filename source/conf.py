6005c7715f9427be4f85e446c0ec746e60bde546
valbendan <valbendan@hotmail.com>
2022-02-11 10:11:15 +0800

    configuration

--- source/conf.py
+++ source/conf.py
@@ -21,16 +21,27 @@ project = "Typed Ansible"
 copyright = "2022, valbendan"
 author = "valbendan"
 
+# 版本配置
+version = "v22.2.11"
 # The full version, including alpha/beta/rc tags
 release = "2022.2.11"
 
-
 # -- General configuration ---------------------------------------------------
+
+# for sitemap remove lang and version
+html_baseurl = "https://jb.2cc.net"
+sitemap_url_scheme = "{link}"
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
+extensions = [
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.todo",
+    "sphinx_sitemap",
+    "sphinx_rtd_theme",
+    "notfound.extension",
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
@@ -40,15 +51,24 @@ templates_path = ["_templates"]
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
+# https://sphinx-notfound-page.readthedocs.io/en/latest/configuration.html
+notfound_no_urls_prefix = True
+
+# sphinx.ext.todo
+# https://www.sphinx-doc.org/en/master/usage/extensions/todo.html#confval-todo_includ
+todo_include_todos = True
 
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "alabaster"
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+
+# 不允许查看源代码
+html_show_sourcelink = False
